8c1c1a7848085a26541d5751c3ab9a9b98863472
PixelTom <tom@pixeltom.net>
2017-06-12 21:19:28 +1000

    Properties update

--- src/js/game/properties.js
+++ src/js/game/properties.js
@@ -1,15 +1,15 @@
 var npmProperties = require('../../../package.json');
 
 module.exports = {
-  title: 'Phaser JS Boilerplate',
+  title: 'DM Tools',
   description: npmProperties.description,
   port: 3017,
   liveReloadPort: 3018,
   mute: false,
   showStats: true,
   size: {
-    x: 800,
-    y: 600
+    x: 1024,
+    y: 768
   },
-  analyticsId: 'UA-50892214-2'
+  //analyticsId: 'UA-50892214-2'
 };
